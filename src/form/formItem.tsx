91302375b9e65281004907c30f797412976c1d33
Muyunyun <328375795@qq.com>
2018-12-09 12:19:00 +0800

    todo: 表单独立渲染

--- src/form/formItem.tsx
+++ src/form/formItem.tsx
@@ -3,11 +3,53 @@ import * as classnames from 'classnames'
 import FormContext from '../core/Context'
 import './index.less'
 
+class FormContent extends React.PureComponent<any, any> {
+  render() {
+    const {
+      label,
+      colon = true,
+      labelCol = 8,
+      wrapCol = 16,
+      inline = false,
+      // errorMsg,
+      formItem,
+    } = this.props
+    console.log('FormContent')
+    return (
+      <div className={classnames({
+        'reform-item': true,
+        'reform-item-inline': inline
+      })}>
+        <section className={classnames({ // label
+          'reform-item-label': true,
+          [`col-${labelCol}`]: true,
+        })}>
+          <span className={classnames({
+            'reform-item-label-text': true,
+          })}>{label}{colon ? ':' : ''}</span>
+        </section>
+        <section className={classnames({ // wrap
+          'reform-item-wrap': true,
+          [`col-${wrapCol}`]: true
+        })}>
+          <div>{formItem}</div>
+          {/* <div className="reform-item-wrap-error">{errorMsg}</div> */}
+        </section>
+      </div>
+    )
+  }
+}
+
 class FormElement extends React.PureComponent<any, any> {
+  static formItem = null
   static contextType = FormContext
 
-  state = {
-    value: ''
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      value: '',
+    }
   }
 
   onChange = (e: any) => {
@@ -19,29 +61,34 @@ class FormElement extends React.PureComponent<any, any> {
     this.context.changeFormData(this.context.formData)
   }
 
+  // getDerivedStateFromProps(nextProps: any, prevState: any) {
+  //   if (nextProps.value !== prevState)
+  // }
+
   render() {
+    let errorMsg = ''
     const {
-      children,
       name,
       label,
-      colon = true,
-      labelCol = 8,
-      wrapCol = 16,
-      inline = false,
+      colon,
+      labelCol,
+      wrapCol,
+      inline,
+      children,
     } = this.props
-    const childrenAsElement = children as React.ReactElement<any>
 
-    let formItem: any
-    const props = {
-      value: this.context.formData.getFormItem(name),
+    const value = this.context.formData.getFormItem(name)
+
+    const childrenAsElement = children as React.ReactElement<any>
+    const formProps = {
+      value,
       onChange: this.onChange,
     }
     if (React.Children.only(children)) {
-      formItem = React.cloneElement(childrenAsElement, props)
+      FormElement.formItem = React.cloneElement(childrenAsElement, formProps)
     } else {
       throw new Error('There is must a form element after FormItem')
     }
-    let errorMsg = ''
 
     return (
       <FormContext.Consumer>
@@ -49,27 +96,17 @@ class FormElement extends React.PureComponent<any, any> {
           if (global.errorInfo && global.errorInfo[name] && global.errorInfo[name][0]) {
             errorMsg = global.errorInfo[name][0].message
           }
+          console.log('FormContext.Consumer', this.props)
           return (
-            <div className={classnames({
-              'reform-item': true,
-              'reform-item-inline': inline
-            })}>
-              <section className={classnames({ // label
-                'reform-item-label': true,
-                [`col-${labelCol}`]: true,
-              })}>
-                <span className={classnames({
-                  'reform-item-label-text': true,
-                })}>{label}{colon ? ':' : ''}</span>
-              </section>
-              <section className={classnames({ // wrap
-                'reform-item-wrap': true,
-                [`col-${wrapCol}`]: true
-              })}>
-                <div>{formItem}</div>
-                <div className="reform-item-wrap-error">{errorMsg}</div>
-              </section>
-            </div>
+            <FormContent
+              label={label}
+              colon={colon}
+              labelCol={labelCol}
+              wrapCol={wrapCol}
+              inline={inline}
+              // errorMsg={errorMsg}
+              formItem={FormElement.formItem}
+            />
           )
         }}
       </FormContext.Consumer>
